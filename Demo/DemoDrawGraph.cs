935449823eaa8234fb509cc1a901cb9f32329a8d
Vlad <vladik.voina@gmail.com>
2019-10-16 17:58:51 +1100

    Added comments to demo script

--- Demo/DemoDrawGraph.cs
+++ Demo/DemoDrawGraph.cs
@@ -10,36 +10,58 @@ namespace UnityGraphs.Demo
     {
         public bool updateGraph = false;
 
-        public Rigidbody body;
-
         void Start()
         {
+            //Combine two graphs in the same view 
             DrawGraph.Get("c1").SetGroup("GroupC");
             DrawGraph.Get("c2").SetGroup("GroupC");
 
             // Supported types: float, Quaternion, Vector3
-            DrawGraph.Add( "Vec3Test", Vector3.zero ).ForEach( graph => graph
-                .SetLineWidth( 2f )
-                .SetGraphHeight( 120f )
-                .SetStepSize( 0.25f )
-            );
+            DrawGraph.Add( "Vec3Test", Vector3.zero )
+                // Use System.Linq::ForEach( ... ) to assing group style values
+                .ForEach( graph => graph
+                    .SetLineWidth( 2f )
+                    .SetGraphHeight( 120f )
+                    .SetStepSize( 0.25f )
+                );
         }
 
         void Update()
         {
             if( ! updateGraph ) return;
 
-            DrawGraph.Add( "A", Random.value );
+            // Add values to be drawn in the graph
+            // ( seperate graphs, not in a group )
+            // Since no settings are assigned, default style
+            // values are used for A & B
+            DrawGraph.Add( "A", Random.value ); 
             DrawGraph.Add( "B", Random.value );
 
+            // Automaticlly create a group for Vector3 and 
+            // assign each of the x,y,z componenets into the
+            // same group
             DrawGraph.Add( "Vec3Test", new Vector3(
                 Mathf.Sin( Time.time / 10 ),
                 Mathf.Tan( 1 / Time.time ),
                 Mathf.Cos( Time.time / 10 )
             ) );
 
+            // c1 & c2 will be drawn in the same group
             DrawGraph.Add( "c1", Mathf.Sin( 1 / Time.time ) );
-            DrawGraph.Add( "c2", Mathf.Tan( 1 / Time.time ) );
+
+            // If you are feeling particulary lazy, style values 
+            // can be assined during update
+            DrawGraph.Add( "Random red", Random.value ).SetLengthLimit( 250 )
+                .SetColor( Color.red )
+                .SetLineWidth( 4f )
+                .SetStepSize( 2f );
+
+            // Grouped items don't need to be updated in the same place or time
+            if( Random.value > 0.7 )
+            {
+                // c1 & c2 will be drawn in the same group
+                DrawGraph.Add( "c2", Mathf.Tan( 1 / Time.time ) );
+            }
         }
     }
 }
